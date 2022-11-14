c4d26cf4d7264cc05491d53f17a3f177e56d5ecf
bolinx2003 <3154756364@qq.com>
2022-11-14 14:34:22 +0800

    使用链表实现队列

---
+++ Queue/Queue/Queue.c
@@ -0,0 +1,107 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "Queue.h"
+
+void QueueInit(Queue* pq)
+{
+	assert(pq);
+
+	pq->head = pq->tail = NULL;
+}
+
+void QueueDestroy(Queue* pq)
+{
+	assert(pq);
+
+	QNode* cur = pq->head;
+	while (cur)
+	{
+		QNode* next = cur->next;
+		free(cur);
+		cur = next;
+	}
+
+	pq->head = pq->tail = NULL;
+}
+
+void QueuePush(Queue* pq, QDataType x)
+{
+	assert(pq);
+
+	QNode* newnode = (QNode*)malloc(sizeof(QNode));
+	if (newnode == NULL)
+	{
+		perror("QueuePush::malloc");
+		return;
+	}
+	newnode->data = x;
+	newnode->next = NULL;
+
+	if (pq->tail == NULL)
+	{
+		pq->head = pq->tail = newnode;
+	}
+	else
+	{
+		pq->tail->next = newnode;
+		pq->tail = newnode;
+	}
+}
+
+void QueuePop(Queue* pq)
+{
+	assert(pq);
+	assert(!QueueEmpty(pq));
+
+	if (pq->head->next == NULL)
+	{
+		// 只剩1个结点
+		free(pq->head);
+		pq->head = pq->tail = NULL;
+	}
+	else
+	{
+		// 还有多个结点
+		QNode* next = pq->head->next;
+		free(pq->head);
+		pq->head = next;
+	}
+}
+
+QDataType QueueFront(const Queue* pq)
+{
+	assert(pq);
+	assert(!QueueEmpty(pq));
+
+	return pq->head->data;
+}
+
+QDataType QueueBack(const Queue* pq)
+{
+	assert(pq);
+	assert(!QueueEmpty(pq));
+
+	return pq->tail->data;
+}
+
+bool QueueEmpty(const Queue* pq)
+{
+	assert(pq);
+
+	return pq->head == NULL;
+}
+
+int QueueSize(const Queue* pq)
+{
+	assert(pq);
+
+	int size = 0;
+	QNode* cur = pq->head;
+	while (cur)
+	{
+		++size;
+		cur = cur->next;
+	}
+
+	return size;
+}
---
+++ Queue/Queue/Test.c
@@ -0,0 +1,40 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "Queue.h"
+
+void TestQueue()
+{
+	Queue q;
+
+	// 初始化
+	QueueInit(&q);
+
+	// 入队列：1~10
+	for (int i = 1; i <= 10; i++)
+	{
+		QueuePush(&q, i);
+		printf("%d ", QueueBack(&q));
+	}
+	printf("\n");
+
+	// 计算队列元素个数
+	printf("队列元素个数：%d\n", QueueSize(&q));
+
+	// 出队列
+	while (!(QueueEmpty(&q)))
+	{
+		printf("%d ", QueueFront(&q));
+		QueuePop(&q);
+	}
+	printf("\n");
+
+	// 销毁
+	QueueDestroy(&q);
+}
+
+int main()
+{
+	TestQueue();
+
+	return 0;
+}
