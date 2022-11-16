b77dde38004692155925574045fe1e83535dd639
bolinx2003 <3154756364@qq.com>
2022-11-16 17:54:10 +0800

    堆

---
+++ Heap/Heap/Heap.c
@@ -0,0 +1,77 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "Heap.h"
+
+void HeapInit(HP* php)
+{
+	assert(php);
+	php->a = NULL;
+	php->size = php->capacity = 0;
+}
+
+void HeapDestroy(HP* php)
+{
+	assert(php);
+
+	free(php->a);
+	php->a = NULL;
+	php->size = php->capacity = 0;
+}
+
+static void Swap(HPDataType* p1, HPDataType* p2)
+{
+	assert(p1 && p2);
+
+	HPDataType tmp = *p1;
+	*p1 = *p2;
+	*p2 = tmp;
+}
+
+static void AdjustUp(HPDataType* a, int child)
+{
+	assert(a);
+
+	int parent = (child - 1) / 2;
+	while (child > 0)
+	{
+		if (a[child] > a[parent])
+		{
+			Swap(a + child, a + parent);
+			child = parent;
+			parent = (child - 1) / 2;
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+
+void HeapPush(HP* php, HPDataType x)
+{
+	assert(php);
+
+	// 检查容量，满了要扩容
+	if (php->size == php->capacity)
+	{
+		int newCapacity = php->capacity == 0 ? 4 : 2 * php->capacity;
+		HPDataType* tmp = (HPDataType*)realloc(php->a, newCapacity * sizeof(HPDataType));
+		if (tmp == NULL)
+		{
+			perror("HeapPush::realloc");
+			return;
+		}
+
+		php->a = tmp;
+		php->capacity = newCapacity;
+	}
+
+	php->a[php->size++] = x;
+
+	AdjustUp(php->a, php->size - 1);
+}
+
+void HeapPop(HP* php);
+HPDataType HeapTop(const HP* php);
+bool HeapEmpty(const HP* php);
+int HeapSize(const HP* php);
---
+++ Heap/Heap/Heap.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+
+typedef int HPDataType;
+
+typedef struct Heap
+{
+	HPDataType* a;
+	int size;
+	int capacity;
+}HP;
+
+void HeapInit(HP* php);
+void HeapDestroy(HP* php);
+void HeapPush(HP* php, HPDataType x);
+void HeapPop(HP* php);
+HPDataType HeapTop(const HP* php);
+bool HeapEmpty(const HP* php);
+int HeapSize(const HP* php);
