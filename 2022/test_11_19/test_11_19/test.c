313317b7a3fa03679414f2343cd0497a7d29c061
bolinx2003 <3154756364@qq.com>
2022-11-19 19:04:25 +0800

    提交代码

---
+++ 2022/test_11_19/test_11_19/test.c
@@ -0,0 +1,58 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdbool.h>
+
+struct TreeNode
+{
+	int val;
+	struct TreeNode* left;
+	struct TreeNode* right;
+};
+
+// 判断是否是单值二叉树
+//
+//bool flag = true;
+//
+//void PreOrderCompare(struct TreeNode* root, int val)
+//{
+//	if (root == NULL)
+//		return;
+//
+//	if (flag == false)
+//		return;
+//
+//	if (root->val != val)
+//	{
+//		flag = false;
+//		return;
+//	}
+//
+//	PreOrderCompare(root->left, val);
+//	PreOrderCompare(root->right, val);
+//}
+//
+//bool isUnivalTree(struct TreeNode* root)
+//{
+//	if (root == NULL)
+//		return true;
+//
+//	flag = true;
+//	PreOrderCompare(root, root->val);
+//	return flag;
+//}
+//
+//bool isUnivalTree(struct TreeNode* root)
+//{
+//	if (root == NULL)
+//		return true;
+//
+//	if (root->left && root->left->val != root->val)
+//		return false;
+//
+//	if (root->right && root->right->val != root->val)
+//		return false;
+//
+//	return isUnivalTree(root->left)
+//		&& isUnivalTree(root->right);
+//}
